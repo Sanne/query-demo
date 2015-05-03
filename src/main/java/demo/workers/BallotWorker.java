6ea25dc5e02e51a548b2588a93afdb34ea7d07fb
navssurtani <navssurtani@gmail.com>
2015-05-03 16:09:26 +0100

    Added in Jackson. Removed quick-json

--- src/main/java/demo/workers/BallotWorker.java
+++ src/main/java/demo/workers/BallotWorker.java
@@ -1,7 +1,7 @@
 package demo.workers;
 
-import com.codesnippets4all.json.parsers.JSONParser;
-import com.codesnippets4all.json.parsers.JsonParserFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.jboss.logging.Logger;
 
 import java.util.Map;
 
@@ -10,19 +10,37 @@ import java.util.Map;
  * This will then perform the work of creating beans and putting them into
  * the data grid.
  *
+ * JSON is of the form:
+ *
+ * {
+ *    "name":"voterName",
+ *    "age":"voterAge",
+ *    "governorVote":"governorVoteName",
+ *    "senatorVote":"senatorVoteName",
+ *    "regionSelection":1
+ *    }
+ *
  * @author - @navssurtani
  */
 public class BallotWorker {
 
+   private static final Logger logger = Logger.getLogger(BallotWorker.class);
+   private ObjectMapper mapper;
+
+
+   public BallotWorker() {
+      // Instantiate fields.
+      mapper = new ObjectMapper();
+   }
+
+
    public void work(String data) throws Exception {
+      // Parse the JSON String into a Map<String, Object>
+      Map<String, Object> parsed = mapper.readValue(data, Map.class);
+      logger.log(Logger.Level.DEBUG, "Parsed String into Map using JSON " +
+            "parser");
 
-      // Parse the string (json) and build the beans from there.
-      JsonParserFactory factory = JsonParserFactory.getInstance();
-      JSONParser parser = factory.newJsonParser();
-
-      Map jsonMap = parser.parseJson(data);
-
-      // Put the data into the grid.
+      // Now we need to put the data into the grid.
 
    }
 
