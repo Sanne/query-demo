1664fdf86e78fb1af77085a8bc5271d96ae34ce7
Sanne Grinovero <sanne@hibernate.org>
2015-05-03 21:58:39 +0100

    Making some adjustments to BallotWorkerTest: using tabs and changed name of attribute to 'senateVote'

--- src/test/java/BallotWorkerTest.java
+++ src/test/java/BallotWorkerTest.java
@@ -1,6 +1,8 @@
 import demo.workers.BallotWorker;
+
 import org.infinispan.Cache;
 import org.infinispan.manager.DefaultCacheManager;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,31 +14,33 @@ import java.io.IOException;
  */
 public class BallotWorkerTest {
 
-   @BeforeClass
-   public static void startInfinispan() throws IOException {
-      DefaultCacheManager defaultCacheManager =
-            new DefaultCacheManager("localonly-infinispan.xml");
-      Cache testCache = defaultCacheManager.getCache("testCache", true);
+	private static DefaultCacheManager defaultCacheManager;
 
+	@BeforeClass
+	public static void startInfinispan() throws IOException {
+		defaultCacheManager = new DefaultCacheManager( "localonly-infinispan.xml" );
+		Cache testCache = defaultCacheManager.getCache( "testCache", true );
+	}
 
-   }
+	@AfterClass
+	public static void stopInfinispan() {
+		if (defaultCacheManager != null)
+			defaultCacheManager.stop();
+	}
 
-   @Test
-   public void testCorrectJson(){
-      String correct = "{\"name\":\"Another\",\"age\":\"75\"," +
-            "\"governorVote\":\"Stewie Griffin\"," +
-            "\"senatorVote\":\"Homer Simpson\"," +
-            "\"regionSelection\":2}";
-      BallotWorker ballotWorker = new BallotWorker();
+	@Test
+	public void testCorrectJson() {
+		String correct = "{\"name\":\"Another\",\"age\":\"75\"," + "\"governorVote\":\"Stewie Griffin\"," + "\"senateVote\":\"Homer Simpson\","
+				+ "\"regionSelection\":2}";
+		BallotWorker ballotWorker = new BallotWorker();
 
-      try {
-         ballotWorker.work(correct);
-      } catch (Exception e) {
-         e.printStackTrace();
-         Assert.fail("There shouldn't be an exception here. The worker should " +
-               "run smoothly");
-      }
-   }
-
+		try {
+			ballotWorker.work( correct );
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail( "There shouldn't be an exception here. The worker should " + "run smoothly" );
+		}
+	}
 
 }
